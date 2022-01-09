660f63741cfe50934f2753229f5422871655240f
Andrew El <laingas@gmail.com>
2022-01-09 15:09:21 -0500

    index creation app example

---
+++ Akka.Persistence.Linq2Db.IndexHelperApp/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using Akka.Configuration;
+using Akka.Persistence.Linq2Db.IndexHelperLib;
+using Akka.Persistence.Sql.Linq2Db.Config;
+using Akka.Persistence.Sql.Linq2Db.Tests;
+using CommandLine;
+using FluentMigrator.Expressions;
+using FluentMigrator.Runner.Generators;
+using FluentMigrator.Runner.Generators.Generic;
+using FluentMigrator.Runner.Generators.MySql;
+using FluentMigrator.Runner.Generators.Oracle;
+using FluentMigrator.Runner.Generators.Postgres;
+using FluentMigrator.Runner.Generators.Postgres92;
+using FluentMigrator.Runner.Generators.SQLite;
+using FluentMigrator.Runner.Generators.SqlServer;
+using FluentMigrator.Runner.Processors.Postgres;
+using LinqToDB;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Akka.Persistence.Linq2Db.IndexHelperApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(opts =>
+                {
+                    //var str = Linq2DbJournalDefaultSpecConfig.customConfig("testGen",
+                    //    "journalTbl", "metadataTbl", ProviderName.SqlServer,
+                    //    "connStr");
+                    var conf =
+                        ConfigurationFactory.ParseString(
+                            File.ReadAllText(opts.File));
+
+                    var journalConf =
+                        new Akka.Persistence.Sql.Linq2Db.Config.JournalConfig(
+                            conf.GetConfig(
+                                    opts.HoconPath
+                                    //"akka.persistence.journal.linq2db.testGen"
+                                    )
+                                .WithFallback(Akka.Persistence.Sql.Linq2Db
+                                    .Journal
+                                    .Linq2DbWriteJournal.DefaultConfiguration));
+                    var generator = getGenerator(journalConf.ProviderName);
+                    var helper = new JournalIndexHelper();
+                    CreateIndexExpression expr = null;
+                    GeneratePerOptions(opts, helper, journalConf, generator);
+                });
+        }
+
+        private static void GeneratePerOptions(Options opts, JournalIndexHelper helper,
+            JournalConfig journalConf, GenericGenerator generator)
+        {
+            CreateIndexExpression expr;
+            if (opts.GeneratePidSeqNo)
+            {
+                expr = new CreateIndexExpression()
+                {
+                    Index = helper.JournalOrdering(journalConf.TableConfig.TableName,
+                        journalConf.TableConfig.ColumnNames.Ordering,
+                        journalConf.TableConfig.SchemaName)
+                };
+                GenerateWithHeaderAndFooter(generator, expr, "Ordering");
+            }
+
+            if (opts.GeneratePidSeqNo)
+            {
+                expr = new CreateIndexExpression()
+                {
+                    Index = helper.DefaultJournalIndex(
+                        journalConf.TableConfig.TableName,
+                        journalConf.TableConfig.ColumnNames.PersistenceId,
+                        journalConf.TableConfig.ColumnNames.SequenceNumber,
+                        journalConf.TableConfig.SchemaName)
+                };
+                GenerateWithHeaderAndFooter(generator, expr, "PidAndSequenceNo");
+            }
+
+            if (opts.GenerateTimestamp)
+            {
+                expr = new CreateIndexExpression()
+                {
+                    Index = helper.JournalTimestamp(journalConf.TableConfig.TableName,
+                        journalConf.TableConfig.ColumnNames.Created,
+                        journalConf.TableConfig.SchemaName)
+                };
+                GenerateWithHeaderAndFooter(generator, expr, "Timestamp");
+            }
+        }
+
+        private static void GenerateWithHeaderAndFooter(GenericGenerator generator,
+            CreateIndexExpression expr, string indexType)
+        {
+            Console.WriteLine("-------");
+            Console.WriteLine($"----{indexType} Index Create Below");
+            Console.WriteLine(generator.Generate(expr));
+            Console.WriteLine($"----{indexType} Index Create Above");
+            Console.WriteLine("-------");
+        }
+
+        static GenericGenerator getGenerator(string dbArg)
+        {
+            if (dbArg.StartsWith("sqlserver",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new SqlServer2008Generator();
+            }
+            else if (dbArg.Contains("sqlite",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new SQLiteGenerator();
+            }
+            else if (dbArg.Contains("postgres",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new Postgres92Generator(
+                    new PostgresQuoter(new PostgresOptions()),
+                    new OptionsWrapper<GeneratorOptions>(
+                        new GeneratorOptions()));
+            }
+            else if (dbArg.Contains("mysql",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new MySql5Generator();
+            }
+            else if (dbArg.Contains("oracle",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new OracleGenerator();
+            }
+            else
+            {
+                throw new Exception("IDK what to do with this!");
+            }
+        }
+    }
+}
---
+++ Akka.Persistence.Linq2Db.IndexHelperLib/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentMigrator.Model;
+
+namespace Akka.Persistence.Linq2Db.IndexHelperLib
+{
+    public class JournalIndexHelper
+    {
+        public IndexDefinition DefaultJournalIndex(string tableName, string persistenceIdCol, string sequenceNoCol, string schemaName = null)
+        {
+            var idx =  beginCreateIndex(tableName, schemaName, $"UX_{tableName}_PID_SEQNO");
+            //short name for easy compat with all dbs. (*cough* oracle *cough*)
+            idx.Columns.Add(new IndexColumnDefinition(){ Name = persistenceIdCol });
+            idx.Columns.Add(new IndexColumnDefinition(){Name = sequenceNoCol, Direction = Direction.Ascending});
+            idx.IsUnique = true;
+            return idx;
+        }
+
+        public IndexDefinition JournalOrdering(string tableName,
+            string orderingCol, string schemaName = null)
+        {
+            var idx =  beginCreateIndex(tableName, schemaName,$"IX_{tableName}_Ordering");
+            idx.Columns.Add(new IndexColumnDefinition(){Name = orderingCol});
+            //Should it be?
+            //idx.IsUnique = true;
+            return idx;
+        }
+
+        public IndexDefinition JournalTimestamp(string tableName,
+            string timestampCol, string schemaName = null)
+        {
+            var idx = beginCreateIndex(tableName, schemaName,
+                $"IX_{tableName}_TimeStamp");
+            idx.Columns.Add(new IndexColumnDefinition(){Name = timestampCol});
+            //Not unique by any stretch.
+            return idx;
+        }
+
+        private static IndexDefinition beginCreateIndex(string tableName, string schemaName, string indexName)
+        {
+            var idx = new IndexDefinition();
+            if (string.IsNullOrWhiteSpace(schemaName) == false)
+            {
+                idx.SchemaName = schemaName;
+            }
+            idx.TableName = tableName;
+            idx.Name = indexName;
+            return idx;
+        }
+    }
+}
