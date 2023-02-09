554f660b655b953138e79909d6d384f6877aabd8
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-02-10 02:51:51 +0700

    Remove byte rot

--- src/Akka.Persistence.Sql.Linq2Db/Query/Dao/BaseByteReadArrayJournalDAO.cs
+++ src/Akka.Persistence.Sql.Linq2Db/Query/Dao/BaseByteReadArrayJournalDAO.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Akka.Actor;
@@ -145,7 +144,6 @@ namespace Akka.Persistence.Sql.Linq2Db.Query.Dao
                         .Via(_deserializeFlow);
                 
                 case TagReadMode.TagTable:
-                    //return EventByTagTableOnly(tag, offset, maxOffset, separator, maxTake);
                     return AsyncSource<JournalRow>.FromEnumerable(
                             new { separator, tag, offset, maxOffset, maxTake, ConnectionFactory },
                             async input =>
@@ -172,107 +170,6 @@ namespace Akka.Persistence.Sql.Linq2Db.Query.Dao
                     throw new ArgumentOutOfRangeException();
             }
         }
-
-        private Source<Try<(IPersistentRepresentation, IImmutableSet<string>, long)>, NotUsed> EventByTagTableOnly(
-            string tag,
-            long offset,
-            long maxOffset,
-            string separator,
-            int maxTake)
-        {
-            return AsyncSource<JournalRow>
-                .FromEnumerable(
-                    new { ConnectionFactory, separator, tag, offset, maxOffset, maxTake },
-                    async input =>
-                    {
-                        await using var conn = input.ConnectionFactory.GetConnection();
-                        var journalTable = conn.GetTable<JournalRow>();
-                        var tagTable = conn.GetTable<JournalTagRow>();
-                        var query =
-                            from r in journalTable
-                            from lp in tagTable.Where(jtr => 
-                                jtr.OrderingId == r.Ordering 
-                                && jtr.PersistenceId == r.PersistenceId).DefaultIfEmpty()
-                            where lp.OrderingId > input.offset
-                                  && lp.OrderingId <= input.maxOffset
-                                  && !r.Deleted
-                                  && lp.TagValue == input.tag
-                            select r;
-                        var mainRows = await query.ToListAsync();
-                        await AddTagDataFromTagTable(mainRows, conn);
-                        return mainRows;
-                        
-                        /*
-                        //First, Get eligible rows.
-                        var mainRows = await conn.GetTable<JournalRow>()
-                                .LeftJoin(
-                                    conn.GetTable<JournalTagRow>(),
-                                    (jr, jtr) => jr.Ordering == jtr.JournalOrderingId,
-                                    (jr, jtr) => new { jr, jtr })
-                                .Where(r => r.jtr.TagValue == input.tag)
-                                .Select(r => r.jr)
-                                .Where(r => r.Ordering > input.offset && r.Ordering <= input.maxOffset && !r.Deleted)
-                                .Take(input.maxTake).ToListAsync();
-                        await AddTagDataFromTagTable(mainRows, conn);
-                        return mainRows;
-                        */
-                    })
-                //We still PerfectlyMatchTag here
-                //Because DB Collation :)
-                //.Via(PerfectlyMatchTag(tag, separator))
-                .Via(_deserializeFlow);
-        }
-
-        /*
-        private Source<Try<(IPersistentRepresentation, IImmutableSet<string>, long)>, NotUsed> EventByTagMigration(
-            string tag,
-            long offset,
-            long maxOffset,
-            string separator,
-            int maxTake)
-        {
-            return AsyncSource<JournalRow>.FromEnumerable(
-                    new { ConnectionFactory, separator, tag, offset, maxOffset, maxTake },
-                    async input =>
-                    {
-                        // NOTE: This flow is probably not performant,
-                        // It is meant to allow for safe migration
-                        // And is not necessarily intended for long term use
-                        await using var conn = input.ConnectionFactory.GetConnection();
-                        
-                        var tagValue = $"{separator}{tag}{separator}";
-                        // First, find the rows.
-                        // We use IN here instead of left join because it's safer from a
-                        // 'avoid duplicate rows tripping things up later' standpoint.
-                        var mainRows = await conn.GetTable<JournalRow>()
-                            .Where(r => 
-                                r.Tags.Contains(tagValue) ||
-                                r.Ordering.In(
-                                    conn.GetTable<JournalTagRow>()
-                                        .Where(j => j.TagValue == tag)
-                                        .Select(j => j.OrderingId)))
-                            .OrderBy(r => r.Ordering)
-                            .Where(r => r.Ordering > input.offset && r.Ordering <= input.maxOffset && r.Deleted == false)
-                            .Take(input.maxTake).ToListAsync();
-                        
-                        await AddTagDataFromTagTable(mainRows, conn);
-                        return mainRows;
-                    })
-                .Via(_deserializeFlow);
-        }        
-        
-        private Flow<JournalRow, JournalRow, NotUsed> PerfectlyMatchTag(
-            string tag,
-            string separator)
-        {
-            //Do the tagArr check first here
-            //Since the logic is simpler.
-            return Flow.Create<JournalRow>()
-                .Where(r => r.TagArr?.Contains(tag) ?? (r.Tags ?? "")
-                    .Split( new[] { separator }, StringSplitOptions.RemoveEmptyEntries )
-                    .Any(t => t.Contains(tag)));
-        }
-        */
 
         public override Source<Try<ReplayCompletion>, NotUsed> Messages(
             DataConnection dc, 
