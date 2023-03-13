2fc5c188a5a09e283c2bdf697bf59f8ee42f45ac
David Cumps <david@cumps.be>
2023-03-14 00:54:47 +0100

    style: cleanup Akka.Persistence.Sql.TagTableMigration

--- src/Akka.Persistence.Sql.TagTableMigration/Options.cs
+++ src/Akka.Persistence.Sql.TagTableMigration/Options.cs
@@ -4,78 +4,78 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
-using Akka.Configuration;
+using LinqToDB;
 
-namespace Akka.Persistence.Sql.TagTableMigration;
-
-public enum DatabaseType
+namespace Akka.Persistence.Sql.TagTableMigration
 {
-    SqlServer,
-    Sqlite,
-    PostgreSql,
-    MySql
-}
+    public enum DatabaseType
+    {
+        SqlServer,
+        Sqlite,
+        PostgreSql,
+        MySql
+    }
 
-public enum ProviderType
-{
-    SqlServer,
-    SqlServer2000,
-    SqlServer2005,
-    SqlServer2008,
-    SqlServer2012,
-    SqlServer2014,
-    SqlServer2016,
-    SqlServer2017,
-    MySql,
-    MySqlOfficial,
-    MySqlConnector,
-    PostgreSql,
-    PostgreSql92,
-    PostgreSql93,
-    PostgreSql95,
-    Sqlite,
-    SqliteClassic,
-    SqliteMs
-}
+    public enum ProviderType
+    {
+        SqlServer,
+        SqlServer2000,
+        SqlServer2005,
+        SqlServer2008,
+        SqlServer2012,
+        SqlServer2014,
+        SqlServer2016,
+        SqlServer2017,
+        MySql,
+        MySqlOfficial,
+        MySqlConnector,
+        PostgreSql,
+        PostgreSql92,
+        PostgreSql93,
+        PostgreSql95,
+        Sqlite,
+        SqliteClassic,
+        SqliteMs
+    }
 
-public static class Extensions
-{
-    public static string ToHocon(this DatabaseType db)
-        => db switch
-        {
-            DatabaseType.SqlServer => "sql-server",
-            DatabaseType.Sqlite => "sqlite",
-            DatabaseType.PostgreSql => "postgresql",
-            DatabaseType.MySql => "mysql",
-            _ => throw new ArgumentException($"Unknown database type: {db}")
-        };
+    public static class Extensions
+    {
+        public static string ToHocon(this DatabaseType db)
+            => db switch
+            {
+                DatabaseType.SqlServer => "sql-server",
+                DatabaseType.Sqlite => "sqlite",
+                DatabaseType.PostgreSql => "postgresql",
+                DatabaseType.MySql => "mysql",
+                _ => throw new ArgumentException($"Unknown database type: {db}")
+            };
 
-    public static string ToHocon(this ProviderType type)
-        => type switch
-        {
-            ProviderType.SqlServer => LinqToDB.ProviderName.SqlServer,
-            ProviderType.SqlServer2000 => LinqToDB.ProviderName.SqlServer2000,
-            ProviderType.SqlServer2005 => LinqToDB.ProviderName.SqlServer2005,
-            ProviderType.SqlServer2008 => LinqToDB.ProviderName.SqlServer2008,
-            ProviderType.SqlServer2012 => LinqToDB.ProviderName.SqlServer2012,
-            ProviderType.SqlServer2014 => LinqToDB.ProviderName.SqlServer2014,
-            ProviderType.SqlServer2016 => LinqToDB.ProviderName.SqlServer2016,
-            ProviderType.SqlServer2017 => LinqToDB.ProviderName.SqlServer2017,
-            ProviderType.MySql => LinqToDB.ProviderName.MySql,
-            ProviderType.MySqlOfficial => LinqToDB.ProviderName.MySqlOfficial,
-            ProviderType.MySqlConnector => LinqToDB.ProviderName.MySqlConnector,
-            ProviderType.PostgreSql => LinqToDB.ProviderName.PostgreSQL,
-            ProviderType.PostgreSql92 => LinqToDB.ProviderName.PostgreSQL92,
-            ProviderType.PostgreSql93 => LinqToDB.ProviderName.PostgreSQL93,
-            ProviderType.PostgreSql95 => LinqToDB.ProviderName.PostgreSQL95,
-            ProviderType.Sqlite => LinqToDB.ProviderName.SQLite,
-            ProviderType.SqliteClassic => LinqToDB.ProviderName.SQLiteClassic,
-            ProviderType.SqliteMs => LinqToDB.ProviderName.SQLiteMS,
-            _ => throw new ArgumentException($"Unknown provider type: {type}")
-        };
+        public static string ToHocon(this ProviderType type)
+            => type switch
+            {
+                ProviderType.SqlServer => ProviderName.SqlServer,
+                ProviderType.SqlServer2000 => ProviderName.SqlServer2000,
+                ProviderType.SqlServer2005 => ProviderName.SqlServer2005,
+                ProviderType.SqlServer2008 => ProviderName.SqlServer2008,
+                ProviderType.SqlServer2012 => ProviderName.SqlServer2012,
+                ProviderType.SqlServer2014 => ProviderName.SqlServer2014,
+                ProviderType.SqlServer2016 => ProviderName.SqlServer2016,
+                ProviderType.SqlServer2017 => ProviderName.SqlServer2017,
+                ProviderType.MySql => ProviderName.MySql,
+                ProviderType.MySqlOfficial => ProviderName.MySqlOfficial,
+                ProviderType.MySqlConnector => ProviderName.MySqlConnector,
+                ProviderType.PostgreSql => ProviderName.PostgreSQL,
+                ProviderType.PostgreSql92 => ProviderName.PostgreSQL92,
+                ProviderType.PostgreSql93 => ProviderName.PostgreSQL93,
+                ProviderType.PostgreSql95 => ProviderName.PostgreSQL95,
+                ProviderType.Sqlite => ProviderName.SQLite,
+                ProviderType.SqliteClassic => ProviderName.SQLiteClassic,
+                ProviderType.SqliteMs => ProviderName.SQLiteMS,
+                _ => throw new ArgumentException($"Unknown provider type: {type}")
+            };
 
-    public static Configuration.Config ToHocon(this Options opt)
-        => (Configuration.Config)$@"
+        public static Configuration.Config ToHocon(this Options opt)
+            => (Configuration.Config)$@"
 akka.persistence {{
 	journal {{
 		plugin = ""akka.persistence.journal.linq2db""
@@ -94,19 +94,20 @@ akka.persistence {{
 		}}
 	}}
 }}";
-}
+    }
 
-public sealed class Options
-{
-    public string? ConnectionString { get; set; }
-    public DatabaseType TableMapping { get; set; }
-    public ProviderType Provider { get; set; }
-    public string? SchemaName { get; set; }
-    public long StartOffset { get; set; }
-    public long? EndOffset { get; set; }
-    public int BatchSize { get; set; }
+    public sealed class Options
+    {
+        public string? ConnectionString { get; set; }
+        public DatabaseType TableMapping { get; set; }
+        public ProviderType Provider { get; set; }
+        public string? SchemaName { get; set; }
+        public long StartOffset { get; set; }
+        public long? EndOffset { get; set; }
+        public int BatchSize { get; set; }
 
-    public override string ToString()
-        => $"ConnectionString: {ConnectionString}, TableMapping: {TableMapping}, Provider: {Provider}, " +
-           $"SchemaName: {SchemaName}, StartOffset: {StartOffset}, EndOffset: {EndOffset}, BatchSize: {BatchSize}";
+        public override string ToString()
+            => $"ConnectionString: {ConnectionString}, TableMapping: {TableMapping}, Provider: {Provider}, " +
+               $"SchemaName: {SchemaName}, StartOffset: {StartOffset}, EndOffset: {EndOffset}, BatchSize: {BatchSize}";
+    }
 }
--- src/Akka.Persistence.Sql.TagTableMigration/Program.cs
+++ src/Akka.Persistence.Sql.TagTableMigration/Program.cs
@@ -7,89 +7,101 @@
 using System.CommandLine;
 using Akka.Persistence.Sql.HelperLib;
 
-namespace Akka.Persistence.Sql.TagTableMigration;
-
-public static class Program
+namespace Akka.Persistence.Sql.TagTableMigration
 {
-    private static readonly Option<string> ConnectionString;
-    private static readonly Option<DatabaseType> TableMapping;
-    private static readonly Option<ProviderType> Provider;
-    private static readonly Option<string> SchemaName;
-    private static readonly Option<long> StartOffset;
-    private static readonly Option<long?> EndOffset;
-    private static readonly Option<int> BatchSize;
-
-    static Program()
+    public static class Program
     {
-        ConnectionString = new Option<string>(
-            aliases: new[] { "--connection-string", "-c" },
-            description: "Database connection string")
+        private static readonly Option<string> ConnectionString;
+        private static readonly Option<DatabaseType> TableMapping;
+        private static readonly Option<ProviderType> Provider;
+        private static readonly Option<string> SchemaName;
+        private static readonly Option<long> StartOffset;
+        private static readonly Option<long?> EndOffset;
+        private static readonly Option<int> BatchSize;
+
+        static Program()
         {
-            IsRequired = true
-        };
-
-        TableMapping = new Option<DatabaseType>(
-            aliases: new[] { "--database", "-d" },
-            description: "Target database being migrated")
-        {
-            IsRequired = true
-        };
-
-        Provider = new Option<ProviderType>(
-            aliases: new[] { "--provider", "-p" },
-            description: "Target database provider")
-        {
-            IsRequired = true
-        };
-
-        SchemaName = new Option<string>(
-            aliases: new[] { "-s", "--schema-name" },
-            description: "Optional. Database schema name");
-
-        StartOffset = new Option<long>(
-            aliases: new[] { "-o", "--offset" },
-            description: "Optional. Starting database row offset",
-            getDefaultValue: () => 0);
-
-        EndOffset = new Option<long?>(
-            aliases: new[] { "-e", "--end-offset" },
-            description: "Optional. Ending database row offset",
-            getDefaultValue: () => null);
-
-        BatchSize = new Option<int>(
-            aliases: new[] { "-b", "--batch-size" },
-            description: "Optional. Batch size per migration transaction",
-            getDefaultValue: () => 1000);
-    }
-
-    public static async Task<int> Main(params string[] args)
-    {
-        var root = new RootCommand("A helper application to migrate legacy Akka.Persistence.Sql database tables to Akka.Persistence.Sql");
-        root.AddOption(ConnectionString);
-        root.AddOption(TableMapping);
-        root.AddOption(Provider);
-        root.AddOption(SchemaName);
-        root.AddOption(StartOffset);
-        root.AddOption(EndOffset);
-        root.AddOption(BatchSize);
-
-        root.SetHandler(async (connectionString, tableMapping, provider, schema, offset, endOffset, batchSize) =>
-        {
-            var opt = new Options
+            ConnectionString = new Option<string>(
+                aliases: new[] { "--connection-string", "-c" },
+                description: "Database connection string")
             {
-                ConnectionString = connectionString,
-                TableMapping = tableMapping,
-                Provider = provider,
-                SchemaName = schema,
-                StartOffset = offset,
-                EndOffset = endOffset,
-                BatchSize = batchSize
+                IsRequired = true
             };
-            var migrator = new TagTableMigrator(opt.ToHocon());
-            await migrator.Migrate(opt.StartOffset, opt.BatchSize, opt.EndOffset);
-        }, ConnectionString, TableMapping, Provider, SchemaName, StartOffset, EndOffset, BatchSize);
 
-        return await root.InvokeAsync(args);
+            TableMapping = new Option<DatabaseType>(
+                aliases: new[] { "--database", "-d" },
+                description: "Target database being migrated")
+            {
+                IsRequired = true
+            };
+
+            Provider = new Option<ProviderType>(
+                aliases: new[] { "--provider", "-p" },
+                description: "Target database provider")
+            {
+                IsRequired = true
+            };
+
+            SchemaName = new Option<string>(
+                aliases: new[] { "-s", "--schema-name" },
+                description: "Optional. Database schema name");
+
+            StartOffset = new Option<long>(
+                aliases: new[] { "-o", "--offset" },
+                description: "Optional. Starting database row offset",
+                getDefaultValue: () => 0);
+
+            EndOffset = new Option<long?>(
+                aliases: new[] { "-e", "--end-offset" },
+                description: "Optional. Ending database row offset",
+                getDefaultValue: () => null);
+
+            BatchSize = new Option<int>(
+                aliases: new[] { "-b", "--batch-size" },
+                description: "Optional. Batch size per migration transaction",
+                getDefaultValue: () => 1000);
+        }
+
+        public static async Task<int> Main(params string[] args)
+        {
+            var root = new RootCommand(
+                "A helper application to migrate legacy Akka.Persistence.Sql database tables to Akka.Persistence.Sql");
+
+            root.AddOption(ConnectionString);
+            root.AddOption(TableMapping);
+            root.AddOption(Provider);
+            root.AddOption(SchemaName);
+            root.AddOption(StartOffset);
+            root.AddOption(EndOffset);
+            root.AddOption(BatchSize);
+
+            root.SetHandler(
+                async (connectionString, tableMapping, provider, schema, offset, endOffset, batchSize) =>
+                {
+                    var opt = new Options
+                    {
+                        ConnectionString = connectionString,
+                        TableMapping = tableMapping,
+                        Provider = provider,
+                        SchemaName = schema,
+                        StartOffset = offset,
+                        EndOffset = endOffset,
+                        BatchSize = batchSize
+                    };
+
+                    var migrator = new TagTableMigrator(opt.ToHocon());
+
+                    await migrator.Migrate(opt.StartOffset, opt.BatchSize, opt.EndOffset);
+                },
+                ConnectionString,
+                TableMapping,
+                Provider,
+                SchemaName,
+                StartOffset,
+                EndOffset,
+                BatchSize);
+
+            return await root.InvokeAsync(args);
+        }
     }
 }
-
