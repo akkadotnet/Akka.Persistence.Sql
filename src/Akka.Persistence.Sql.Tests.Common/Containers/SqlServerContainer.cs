314fb2ecb8d7c9119dce82d03977bd1ac28630e6
David Cumps <david@cumps.be>
2023-03-19 22:27:59 +0100

    chore: bump akka sql to 1.5.1

--- src/Akka.Persistence.Sql.Tests.Common/Containers/SqlServerContainer.cs
+++ src/Akka.Persistence.Sql.Tests.Common/Containers/SqlServerContainer.cs
@@ -7,9 +7,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Akka.Util;
 using Docker.DotNet.Models;
 
