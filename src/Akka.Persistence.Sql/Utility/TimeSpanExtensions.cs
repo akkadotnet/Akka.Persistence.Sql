8626a67e5b4ada78bc913c6d6ebc5b2db9bc4860
David Cumps <david@cumps.be>
2023-03-19 22:18:55 +0100

    chore: bump akka to 1.5.1

---
+++ src/Akka.Persistence.Sql/Utility/TimeSpanExtensions.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="TimeSpanExtensions.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace Akka.Persistence.Sql.Utility
+{
+    /// <summary>
+    ///     TimeSpanExtensions
+    /// </summary>
+    internal static class TimeSpanExtensions
+    {
+        /// <summary>
+        ///     Multiplies a timespan by an integer value
+        /// </summary>
+        internal static TimeSpan Multiply(this TimeSpan multiplicand, int multiplier)
+            => TimeSpan.FromTicks(multiplicand.Ticks * multiplier);
+
+        /// <summary>
+        ///     Multiplies a timespan by a double value
+        /// </summary>
+        internal static TimeSpan Multiply(this TimeSpan multiplicand, double multiplier)
+            => TimeSpan.FromTicks((long)(multiplicand.Ticks * multiplier));
+    }
+}
