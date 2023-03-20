c0a514da2ffd3c261bab5299c87b33e86a6c740a
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-03-21 02:38:07 +0700

    Update Akka versions to v1.5

---
+++ src/Akka.Persistence.Sql/Utility/TimeSpanExtensions.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="TimeSpanExtensions.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace Akka.Persistence.Sql.Utility
+{
+    public static class TimeSpanExtensions
+    {
+        public static TimeSpan Multiply(this TimeSpan timespan, double multiplier)
+            => new ((long)(timespan.Ticks * multiplier));
+    }
+}
