1eea1bdd48982d415c3c471c9db6b73c9d28f1cb
David Cumps <david@cumps.be>
2023-03-21 22:54:53 +0100

    chore: fix copyright header (#169)

--- src/Akka.Persistence.Sql.Benchmark.Tests/SqlJournalPerfSpec.cs
+++ src/Akka.Persistence.Sql.Benchmark.Tests/SqlJournalPerfSpec.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-//  <copyright file="L2dbJournalPerfSpec.cs" company="Akka.NET Project">
+//  <copyright file="SqlJournalPerfSpec.cs" company="Akka.NET Project">
 //      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
 //  </copyright>
 // -----------------------------------------------------------------------
