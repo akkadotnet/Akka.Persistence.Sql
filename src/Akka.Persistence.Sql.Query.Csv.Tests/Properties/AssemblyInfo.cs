a78d7a3fbbe3ebe260654088654499032c3cd9fe
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-03-23 02:16:09 +0700

    Apply custom XUnit framework on csv unit test project

---
+++ src/Akka.Persistence.Sql.Query.Csv.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="AssemblyInfo.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Xunit;
+
+[assembly: TestFramework("Akka.Persistence.Sql.Tests.Internal.Xunit.SqlTestFramework", "Akka.Persistence.Sql.Tests")]
