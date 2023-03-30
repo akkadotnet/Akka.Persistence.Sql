e33c7c04745ae6e9d8bcd9951b73ef56ebc2783b
David Cumps <david@cumps.be>
2023-03-30 16:32:59 +0200

    chore: copyright headers

--- src/Akka.Persistence.Sql.Benchmark.Tests/Sqlite/MsSqliteJournalPerfSpec.cs
+++ src/Akka.Persistence.Sql.Benchmark.Tests/Sqlite/MsSqliteJournalPerfSpec.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-//  <copyright file="SqliteJournalPerfSpec.cs" company="Akka.NET Project">
+//  <copyright file="MsSqliteJournalPerfSpec.cs" company="Akka.NET Project">
 //      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
 //  </copyright>
 // -----------------------------------------------------------------------
@@ -30,7 +30,7 @@ namespace Akka.Persistence.Sql.Benchmark.Tests.Sqlite
         {
             if (!fixture.InitializeDbAsync().Wait(10.Seconds()))
                 throw new Exception("Failed to clean up database in 10 seconds");
-            
+
             return ConfigurationFactory.ParseString(@$"
 akka.persistence {{
     publish-plugin-commands = on
