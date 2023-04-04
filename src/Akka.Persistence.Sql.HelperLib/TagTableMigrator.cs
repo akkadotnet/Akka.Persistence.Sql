edbef9ab8b13318105d43317f98261e1dcf7e33e
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-04-05 01:42:05 +0700

    Clean-up table creation, add indexing to mssql (#196)

--- src/Akka.Persistence.Sql.HelperLib/TagTableMigrator.cs
+++ src/Akka.Persistence.Sql.HelperLib/TagTableMigrator.cs
@@ -52,10 +52,7 @@ namespace Akka.Persistence.Sql.HelperLib
             await using var connection = _connectionFactory.GetConnection();
 
             // Create the tag table if it doesn't exist
-            var dbSchema = connection.GetSchema();
-
-            if (dbSchema.Tables.All(t => t.TableName != _journalConfig.TableConfig.TagTable.Name))
-                await connection.CreateTableAsync<JournalTagRow>();
+            await connection.CreateTableAsync<JournalTagRow>(TableOptions.CreateIfNotExists);
 
             long maxId;
             if (endOffset is null)
