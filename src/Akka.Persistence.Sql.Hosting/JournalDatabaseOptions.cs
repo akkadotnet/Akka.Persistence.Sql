9038424eac4589ec33d87f5a80d6f5a14466498a
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-04-06 02:48:37 +0700

    Add Akka.Hosting extension methods

---
+++ src/Akka.Persistence.Sql.Hosting/JournalDatabaseOptions.cs
@@ -0,0 +1,96 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="DatabaseOptions.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Text;
+using Akka.Hosting;
+
+namespace Akka.Persistence.Sql.Hosting
+{
+    public sealed class JournalDatabaseOptions
+    {
+        public static JournalDatabaseOptions Default => new(DatabaseMapping.Default)
+        {
+            JournalTable = JournalTableOptions.Default,
+            MetadataTable = MetadataTableOptions.Default,
+            TagTable = TagTableOptions.Default
+        };
+
+        public static JournalDatabaseOptions SqlServer => new(DatabaseMapping.SqlServer)
+        {
+            SchemaName = "dbo",
+            JournalTable = JournalTableOptions.SqlServer,
+            MetadataTable = MetadataTableOptions.SqlServer,
+        };
+
+        public static JournalDatabaseOptions Sqlite => new(DatabaseMapping.Sqlite)
+        {
+            JournalTable = JournalTableOptions.Sqlite,
+            MetadataTable = MetadataTableOptions.Sqlite,
+        };
+        
+        public static JournalDatabaseOptions PostgreSql => new(DatabaseMapping.PostgreSql)
+        {
+            SchemaName = "public",
+            JournalTable = JournalTableOptions.PostgreSql,
+            MetadataTable = MetadataTableOptions.PostgreSql,
+        };
+        
+        public static JournalDatabaseOptions MySql => new(DatabaseMapping.MySql)
+        {
+            JournalTable = JournalTableOptions.MySql,
+            MetadataTable = MetadataTableOptions.MySql,
+        };
+
+        public JournalDatabaseOptions(DatabaseMapping mapping)
+        {
+            Mapping = mapping;
+        }
+        
+        public DatabaseMapping Mapping { get; }
+        
+        /// <summary>
+        ///     <para>
+        ///         SQL schema name to table corresponding with persistent journal.
+        ///     </para>
+        ///     <b>Default</b>: <c>null</c>
+        /// </summary>
+        public string? SchemaName { get; set; }
+
+        /// <summary>
+        ///     Journal events table column name mapping
+        /// </summary>
+        public JournalTableOptions? JournalTable { get; set; }
+        
+        /// <summary>
+        ///     Journal metadata table column name mapping, if metadata table is being used
+        /// </summary>
+        public MetadataTableOptions? MetadataTable { get; set; }
+        
+        /// <summary>
+        ///     Event tag table column name mapping, if tag table is being used
+        /// </summary>
+        public TagTableOptions? TagTable { get; set; }
+
+        internal void Build(StringBuilder psb)
+        {
+            var sb = new StringBuilder();
+            if (SchemaName is { })
+                sb.AppendLine($"schema-name = {SchemaName.ToHocon()}");
+            
+            JournalTable?.Build(sb);
+            MetadataTable?.Build(sb);
+            TagTable?.Build(sb);
+
+            if (sb.Length > 0)
+            {
+                psb.AppendLine($"{Mapping.Name()} {{{Environment.NewLine}");
+                psb.Append(sb);
+                psb.AppendLine("}");
+            }
+        }
+    }
+}
---
+++ src/Akka.Persistence.Sql.Hosting/JournalTableOptions.cs
@@ -0,0 +1,165 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="JournalTableOptions.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Text;
+using Akka.Hosting;
+
+namespace Akka.Persistence.Sql.Hosting
+{
+    public sealed class JournalTableOptions
+    {
+        public static JournalTableOptions Default => new()
+        {
+            UseWriterUuidColumn = true,
+            TableName = "journal",
+            
+            OrderingColumnName = "ordering",
+            DeletedColumnName = "deleted",
+            PersistenceIdColumnName = "persistence_id",
+            SequenceNumberColumnName = "sequence_number",
+            CreatedColumnName = "created",
+            TagsColumnName = "tags",
+            MessageColumnName = "message",
+            IdentifierColumnName = "identifier",
+            ManifestColumnName = "manifest",
+            WriterUuidColumnName = "writer_uuid"
+        };
+
+        public static JournalTableOptions SqlServer => new()
+        {
+            UseWriterUuidColumn = false,
+            TableName = "EventJournal",
+            
+            OrderingColumnName = "Ordering",
+            DeletedColumnName = "IsDeleted",
+            PersistenceIdColumnName = "PersistenceId",
+            SequenceNumberColumnName = "SequenceNr",
+            CreatedColumnName = "Timestamp",
+            TagsColumnName = "Tags",
+            MessageColumnName = "Payload",
+            IdentifierColumnName = "SerializerId",
+            ManifestColumnName = "Manifest",
+        };
+        
+        public static JournalTableOptions Sqlite => new()
+        {
+            UseWriterUuidColumn = false,
+            TableName = "event_journal",
+            
+            OrderingColumnName = "ordering",
+            DeletedColumnName = "is_deleted",
+            PersistenceIdColumnName = "persistence_id",
+            SequenceNumberColumnName = "sequence_nr",
+            CreatedColumnName = "timestamp",
+            TagsColumnName = "tags",
+            MessageColumnName = "payload",
+            IdentifierColumnName = "serializer_id",
+            ManifestColumnName = "manifest",
+        };
+        
+        public static JournalTableOptions PostgreSql => new()
+        {
+            UseWriterUuidColumn = false,
+            TableName = "event_journal",
+            
+            OrderingColumnName = "ordering",
+            DeletedColumnName = "is_deleted",
+            PersistenceIdColumnName = "persistence_id",
+            SequenceNumberColumnName = "sequence_nr",
+            CreatedColumnName = "created_at",
+            TagsColumnName = "tags",
+            MessageColumnName = "payload",
+            IdentifierColumnName = "serializer_id",
+            ManifestColumnName = "manifest",
+        };
+        
+        public static JournalTableOptions MySql => new()
+        {
+            UseWriterUuidColumn = false,
+            TableName = "event_journal",
+            
+            OrderingColumnName = "ordering",
+            DeletedColumnName = "is_deleted",
+            PersistenceIdColumnName = "persistence_id",
+            SequenceNumberColumnName = "sequence_nr",
+            CreatedColumnName = "created_at",
+            TagsColumnName = "tags",
+            MessageColumnName = "payload",
+            IdentifierColumnName = "serializer_id",
+            ManifestColumnName = "manifest",
+        };
+        
+        public bool? UseWriterUuidColumn { get; set; }
+        public string? TableName { get; set; }
+        
+        public string? OrderingColumnName { get; set; }
+        public string? DeletedColumnName { get; set; }
+        public string? PersistenceIdColumnName { get; set; }
+        public string? SequenceNumberColumnName { get; set; }
+        public string? CreatedColumnName { get; set; }
+        public string? TagsColumnName { get; set; }
+        public string? MessageColumnName { get; set; }
+        public string? IdentifierColumnName { get; set; }
+        public string? ManifestColumnName { get; set; }
+        public string? WriterUuidColumnName { get; set; }
+
+        internal void Build(StringBuilder psb)
+        {
+            var sb = new StringBuilder();
+
+            if (UseWriterUuidColumn is { })
+                sb.AppendLine($"use-writer-uuid-column = {UseWriterUuidColumn.ToHocon()}");
+
+            if (TableName is { })
+                sb.AppendLine($"table-name = {TableName.ToHocon()}");
+
+            var columnSb = new StringBuilder();
+            if (OrderingColumnName is { })
+                columnSb.AppendLine($"ordering = {OrderingColumnName.ToHocon()}");
+
+            if (DeletedColumnName is { })
+                columnSb.AppendLine($"deleted = {DeletedColumnName.ToHocon()}");
+
+            if (PersistenceIdColumnName is { })
+                columnSb.AppendLine($"persistence-id = {PersistenceIdColumnName.ToHocon()}");
+
+            if (SequenceNumberColumnName is { })
+                columnSb.AppendLine($"sequence-number = {SequenceNumberColumnName.ToHocon()}");
+
+            if (CreatedColumnName is { })
+                columnSb.AppendLine($"created = {CreatedColumnName.ToHocon()}");
+
+            if (TagsColumnName is { })
+                columnSb.AppendLine($"tags = {TagsColumnName.ToHocon()}");
+
+            if (MessageColumnName is { })
+                columnSb.AppendLine($"message = {MessageColumnName.ToHocon()}");
+
+            if (IdentifierColumnName is { })
+                columnSb.AppendLine($"identifier = {IdentifierColumnName.ToHocon()}");
+
+            if (ManifestColumnName is { })
+                columnSb.AppendLine($"manifest = {ManifestColumnName.ToHocon()}");
+
+            if (WriterUuidColumnName is { })
+                columnSb.AppendLine($"writer-uuid = {WriterUuidColumnName.ToHocon()}");
+
+            if (columnSb.Length > 0)
+            {
+                sb.AppendLine("columns {");
+                sb.Append(columnSb);
+                sb.AppendLine("}");
+            }
+
+            if (sb.Length > 0)
+            {
+                psb.AppendLine("journal {");
+                psb.Append(sb);
+                psb.AppendLine("}");
+            }
+        }
+    }
+}
