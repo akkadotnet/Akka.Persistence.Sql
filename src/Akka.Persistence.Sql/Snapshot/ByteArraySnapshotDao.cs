22eaa31e553feb5addaeeda79f2ec39762b933a0
to11mtm <12536917+to11mtm@users.noreply.github.com>
2024-08-11 17:24:36 -0400

    WIP Snapshot Load Batch Queue

--- src/Akka.Persistence.Sql/Snapshot/ByteArraySnapshotDao.cs
+++ src/Akka.Persistence.Sql/Snapshot/ByteArraySnapshotDao.cs
@@ -5,20 +5,49 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using Akka.Event;
 using Akka.Persistence.Sql.Config;
 using Akka.Persistence.Sql.Db;
 using Akka.Persistence.Sql.Extensions;
 using Akka.Streams;
+using Akka.Streams.Dsl;
 using Akka.Util;
 using LinqToDB;
+using LinqToDB.Tools;
 
 namespace Akka.Persistence.Sql.Snapshot
 {
+    public class LatestSnapRequestEntry
+    {
+        public LatestSnapRequestEntry(string persistenceId)
+        {
+            PersistenceId = persistenceId;
+            TCS = new TaskCompletionSource<Option<SelectedSnapshot>>(TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public readonly string PersistenceId;
+        public readonly TaskCompletionSource<Option<SelectedSnapshot>> TCS;
+    }
+
+    public class QueryLatestSnapSet
+    {
+        public readonly Dictionary<string, List<TaskCompletionSource<Option<SelectedSnapshot>>>> Entries = new();
+
+        public void Add(LatestSnapRequestEntry entry)
+        {
+            if (Entries.TryGetValue(entry.PersistenceId, out var item) == false)
+            {
+                item = Entries[entry.PersistenceId] = new List<TaskCompletionSource<Option<SelectedSnapshot>>>();
+            }
+            item.Add(entry.TCS);
+        }
+    }
     public class ByteArraySnapshotDao : ISnapshotDao
     {
         private readonly AkkaPersistenceDataConnectionFactory _connectionFactory;
@@ -29,6 +58,8 @@ namespace Akka.Persistence.Sql.Snapshot
         private readonly CancellationTokenSource _shutdownCts;
         private readonly SnapshotConfig _snapshotConfig;
         private readonly IsolationLevel _writeIsolationLevel;
+        private readonly Channel<LatestSnapRequestEntry> _pendingLatestChannel;
+        private readonly Task<Done> _latestSnapStream;
 
         public ByteArraySnapshotDao(
             AkkaPersistenceDataConnectionFactory connectionFactory,
@@ -48,6 +79,171 @@ namespace Akka.Persistence.Sql.Snapshot
             _readIsolationLevel = snapshotConfig.ReadIsolationLevel;
 
             _shutdownCts = new CancellationTokenSource();
+            _pendingLatestChannel = Channel.CreateUnbounded<LatestSnapRequestEntry>();
+            _latestSnapStream = Source.ChannelReader(_pendingLatestChannel.Reader).BatchWeighted(
+                    50,
+                    a => 1,
+                    e =>
+                    {
+                        var a = new QueryLatestSnapSet();
+                        a.Add(e);
+                        return a;
+                    },
+                    (a, e) =>
+                    {
+                        a.Add(e);
+                        return a;
+                    })
+                .SelectAsync(
+                    1,
+                    async a =>
+                    {
+
+                        using (var connection = _connectionFactory.GetConnection())
+                        {
+                            if (connection.UseDateTime)
+                            {
+                                //TODO: Make this actually work because at some point we split tables, may need to generalize.
+                                var set = await connection.GetTable<LongSnapshotRow>()
+                                    .Where(r => r.PersistenceId.In(a.Entries.Keys))
+                                    .Select(
+                                        r => new
+                                        {
+                                            Created = r.Created,
+                                            PersistenceId = r.PersistenceId,
+                                            SequenceNumber = r.SequenceNumber,
+                                            Manifest = r.Manifest,
+                                            Payload = r.Payload,
+                                            SerializerId = r.SerializerId,
+                                            RowNum = LinqToDB.Sql.Ext.Rank().Over().PartitionBy(r.PersistenceId).OrderByDesc(r.SequenceNumber).ToValue()
+                                        })
+                                    .Where(r => r.RowNum == 1)
+                                    .Select(
+                                        r => new LongSnapshotRow()
+                                        {
+                                            Created = r.Created,
+                                            PersistenceId = r.PersistenceId,
+                                            SequenceNumber = r.SequenceNumber,
+                                            Manifest = r.Manifest,
+                                            Payload = r.Payload,
+                                            SerializerId = r.SerializerId,
+                                        }).ToListAsync();
+                                return (a, set, err: (Exception?)null);
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    var set = await connection.GetTable<LongSnapshotRow>()
+                                        .Where(r => r.PersistenceId.In(a.Entries.Keys))
+                                        .Select(
+                                            r => new
+                                            {
+                                                Created = r.Created,
+                                                PersistenceId = r.PersistenceId,
+                                                SequenceNumber = r.SequenceNumber,
+                                                Manifest = r.Manifest,
+                                                Payload = r.Payload,
+                                                SerializerId = r.SerializerId,
+                                                RowNum = LinqToDB.Sql.Ext.Rank().Over().PartitionBy(r.PersistenceId).OrderByDesc(r.SequenceNumber).ToValue()
+                                            })
+                                        .Where(r => r.RowNum == 1)
+                                        .Select(
+                                            r => new LongSnapshotRow()
+                                            {
+                                                Created = r.Created,
+                                                PersistenceId = r.PersistenceId,
+                                                SequenceNumber = r.SequenceNumber,
+                                                Manifest = r.Manifest,
+                                                Payload = r.Payload,
+                                                SerializerId = r.SerializerId,
+                                            }).ToListAsync();
+                                    return (a, set, err: (Exception?)null);
+                                }
+                                catch (Exception ex)
+                                {
+                                    return (a, null, err: ex);
+                                }
+                            }
+                        }
+                    }).Async().Select(
+                    (ab) =>
+                    {
+                        var (a, b, c) = ab;
+                        if (c != null)
+                        {
+                            foreach (var taskCompletionSourcese in a!.Entries.Values.ToList())
+                            {
+                                foreach (var taskCompletionSource in taskCompletionSourcese)
+                                {
+                                    taskCompletionSource.TrySetException(c);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //TODO: Pool this set:
+                            var tempSet = new List<string>();
+                            if (b.Count == 0)
+                            {
+                                foreach (var keyValuePair in a.Entries)
+                                {
+                                    foreach (var taskCompletionSource in keyValuePair.Value)
+                                    {
+                                        taskCompletionSource.TrySetResult(Option<SelectedSnapshot>.None);
+                                    }
+                                }
+                            }
+                            foreach (var result in b)
+                            {
+                                if (a.Entries.TryGetValue(result.PersistenceId, out var toSet))
+                                {
+                                    try
+                                    {
+                                        var res = _longSerializer.Deserialize(result);
+                                        if (res.IsSuccess)
+                                        {
+                                            foreach (var taskCompletionSource in toSet)
+                                            {
+                                                taskCompletionSource.TrySetResult(res.Success);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            foreach (var taskCompletionSource in toSet)
+                                            {
+                                                taskCompletionSource.TrySetException(res.Failure.Value);
+                                            }
+                                        }
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        foreach (var taskCompletionSource in toSet)
+                                        {
+                                            taskCompletionSource.TrySetException(e);
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    tempSet.Add(result.PersistenceId);
+                                }
+
+                                foreach (var se in tempSet)
+                                {
+                                    if (a.Entries.TryGetValue(se, out var setNo))
+                                    {
+                                        foreach (var taskCompletionSource in setNo)
+                                        {
+                                            taskCompletionSource.TrySetResult(Option<SelectedSnapshot>.None);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        return Done.Instance;
+                    }).RunWith(Sink.Ignore<Done>(), materializer);
         }
 
         public async Task DeleteAllSnapshotsAsync(
@@ -182,41 +378,50 @@ namespace Akka.Persistence.Sql.Snapshot
                 });
         }
 
-        public async Task<Option<SelectedSnapshot>> LatestSnapshotAsync(
+        public Task<Option<SelectedSnapshot>> LatestSnapshotAsync(
             string persistenceId,
             CancellationToken cancellationToken = default)
         {
-            var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _shutdownCts.Token);
-            return await _connectionFactory.ExecuteWithTransactionAsync(
-                _readIsolationLevel,
-                cts.Token,
-                async (connection, token) =>
-                {
-                    if (connection.UseDateTime)
-                    {
-                        var row = await connection
-                            .GetTable<DateTimeSnapshotRow>()
-                            .Where(r => r.PersistenceId == persistenceId)
-                            .OrderByDescending(t => t.SequenceNumber)
-                            .FirstOrDefaultAsync(token);
-
-                        return row != null
-                            ? _dateTimeSerializer.Deserialize(row).Get()
-                            : Option<SelectedSnapshot>.None;
-                    }
-                    else
-                    {
-                        var row = await connection
-                            .GetTable<LongSnapshotRow>()
-                            .Where(r => r.PersistenceId == persistenceId)
-                            .OrderByDescending(t => t.SequenceNumber)
-                            .FirstOrDefaultAsync(token);
-
-                        return row != null
-                            ? _longSerializer.Deserialize(row).Get()
-                            : Option<SelectedSnapshot>.None;
-                    }
-                });
+            var req = new LatestSnapRequestEntry(persistenceId);
+            if (_pendingLatestChannel.Writer.TryWrite(req))
+            {
+                return req.TCS.Task;
+            }
+            else
+            {
+                return Task.FromException<Option<SelectedSnapshot>>(new Exception("Queue is closed, System may be shutting down!"));
+            }
+            //var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _shutdownCts.Token);
+            //return await _connectionFactory.ExecuteWithTransactionAsync(
+            //    _readIsolationLevel,
+            //    cts.Token,
+            //    async (connection, token) =>
+            //    {
+            //        if (connection.UseDateTime)
+            //        {
+            //            var row = await connection
+            //                .GetTable<DateTimeSnapshotRow>()
+            //                .Where(r => r.PersistenceId == persistenceId)
+            //                .OrderByDescending(t => t.SequenceNumber)
+            //                .FirstOrDefaultAsync(token);
+            //
+            //            return row != null
+            //                ? _dateTimeSerializer.Deserialize(row).Get()
+            //                : Option<SelectedSnapshot>.None;
+            //        }
+            //        else
+            //        {
+            //            var row = await connection
+            //                .GetTable<LongSnapshotRow>()
+            //                .Where(r => r.PersistenceId == persistenceId)
+            //                .OrderByDescending(t => t.SequenceNumber)
+            //                .FirstOrDefaultAsync(token);
+            //
+            //            return row != null
+            //                ? _longSerializer.Deserialize(row).Get()
+            //                : Option<SelectedSnapshot>.None;
+            //        }
+            //    });
         }
 
         public async Task<Option<SelectedSnapshot>> SnapshotForMaxTimestampAsync(
