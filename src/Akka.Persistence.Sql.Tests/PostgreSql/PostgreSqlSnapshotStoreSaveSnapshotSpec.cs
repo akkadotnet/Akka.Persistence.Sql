3f03bfda9853bbf3ca42fde072ab92cfd1912963
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-09-06 21:02:58 +0700

    Add SnapshotStore save snapshot spec (#455)
    * Add SnapshotStore save snapshot spec
    
    * Code cleanup
    
    * Cleanup code

---
+++ src/Akka.Persistence.Sql.Tests/PostgreSql/PostgreSqlSnapshotStoreSaveSnapshotSpec.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="PostgreSqlSnapshotStoreSaveSnapshotSpec.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using Akka.Configuration;
+using Akka.Persistence.Sql.Tests.Common.Containers;
+using Akka.Persistence.Sql.Tests.Common.Internal.Xunit;
+using FluentAssertions.Extensions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Sql.Tests.PostgreSql
+{
+#if !DEBUG
+    [SkipWindows]
+#endif
+    [Collection(nameof(PostgreSqlPersistenceSpec))]
+    public class PostgreSqlSnapshotStoreSaveSnapshotSpec: SnapshotStoreSaveSnapshotSpecBase
+    {
+        public PostgreSqlSnapshotStoreSaveSnapshotSpec(ITestOutputHelper output, PostgreSqlContainer fixture)
+            : base(Configuration(fixture), nameof(PostgreSqlSnapshotStoreSaveSnapshotSpec), output)
+        {
+        }
+
+        private static Configuration.Config Configuration(PostgreSqlContainer fixture)
+        {
+            if (!fixture.InitializeDbAsync().Wait(10.Seconds()))
+                throw new Exception("Failed to clean up database in 10 seconds");
+
+            return ConfigurationFactory.ParseString(
+                    $$"""
+                      akka.persistence {
+                          publish-plugin-commands = on
+                          snapshot-store {
+                              plugin = "akka.persistence.snapshot-store.sql"
+                              sql {
+                                  connection-string = "{{fixture.ConnectionString}}"
+                                  provider-name = "{{fixture.ProviderName}}"
+                              }
+                          }
+                      }
+                      """)
+                .WithFallback(SqlPersistence.DefaultConfiguration);
+        }
+    }
+}
---
+++ src/Akka.Persistence.Sql.Tests/SqlServer/SqlServerSnapshotStoreSaveSnapshotSpec.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="SqlServerSnapshotStoreSaveSnapshotSpec.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Persistence.Sql.Tests.Common.Containers;
+using Akka.Persistence.Sql.Tests.Common.Internal.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Sql.Tests.SqlServer
+{
+#if !DEBUG
+    [SkipWindows]
+#endif
+    [Collection(nameof(SqlServerPersistenceSpec))]
+    public class SqlServerSnapshotStoreSaveSnapshotSpec: SnapshotStoreSaveSnapshotSpecBase
+    {
+        public SqlServerSnapshotStoreSaveSnapshotSpec(ITestOutputHelper output, SqlServerContainer fixture)
+            : base(Configuration(fixture), nameof(SqlServerSnapshotStoreSaveSnapshotSpec), output)
+        {
+        }
+
+        private static Configuration.Config Configuration(SqlServerContainer fixture)
+            => SqlServerSnapshotSpecConfig.Create(fixture, "snapshotSpec");
+        
+    }
+}
