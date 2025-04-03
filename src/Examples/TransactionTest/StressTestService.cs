0194c9ebe5a83c8237654138281fdee4cccda708
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-04-04 05:01:01 +0700

    Add transaction test app

---
+++ src/Examples/TransactionTest/StressTestService.cs
@@ -0,0 +1,77 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="StressTestService.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Actor;
+using Microsoft.Extensions.Hosting;
+
+namespace TransactionTest;
+
+public class StressTestService: IHostedService
+{
+    private const int TotalActors = 500;
+    private const int LargePayloadActorCount = 100;
+    private const int LargePayloadSize = 5 * 1024 * 1024;
+    private const int SmallPayloadSize = 5 * 1024;
+    
+    private readonly Random _random = new();
+    private readonly List<IActorRef> _actors = new();
+    private readonly IHostApplicationLifetime _applicationLifetime;
+    private readonly ActorSystem _system;
+    private CancellationTokenSource? _shutdownCts;
+    private PeriodicTimer? _timer;
+    private Task? _timerTask;
+        
+    public StressTestService(ActorSystem system, IHostApplicationLifetime applicationLifetime)
+    {
+        _system = system;
+        _applicationLifetime = applicationLifetime;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        foreach (var i in Enumerable.Range(0, TotalActors))
+        {
+            var actor = i < LargePayloadActorCount 
+                ? _system.ActorOf(Props.Create(() => new TestActor($"p-{i}", LargePayloadSize, _applicationLifetime)), $"p-{i}") 
+                : _system.ActorOf(Props.Create(() => new TestActor($"p-{i}", SmallPayloadSize, _applicationLifetime)), $"p-{i}");
+            _actors.Add(actor);
+        }
+
+        _shutdownCts = new CancellationTokenSource();
+        
+        await Task.Delay(TimeSpan.FromSeconds(10), _shutdownCts.Token);
+        
+        _timer = new PeriodicTimer(TimeSpan.FromMilliseconds(100));
+        _timerTask = Task.Run(
+            async () =>
+            {
+                try
+                {
+                    while (await _timer.WaitForNextTickAsync(_shutdownCts.Token))
+                    {
+                        var index = _random.Next(0, TotalActors);
+                        _actors[index].Tell(SaveEvent.Instance);
+                    }
+                }
+                catch (TimeoutException)
+                {
+                    // no-op
+                }
+                catch (OperationCanceledException)
+                {
+                    // no-op
+                }
+            }, _shutdownCts.Token);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        if(_shutdownCts is not null)
+            await _shutdownCts.CancelAsync();
+        if(_timerTask is not null)
+            await _timerTask;
+    }
+}
---
+++ src/Examples/TransactionTest/TestActor.cs
@@ -0,0 +1,79 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="TestActor.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Event;
+using Akka.Persistence;
+using Microsoft.Extensions.Hosting;
+
+namespace TransactionTest;
+
+public sealed class SaveEvent
+{
+    public static readonly SaveEvent Instance = new ();
+    private SaveEvent() { }
+}
+
+public class TestActor: ReceivePersistentActor
+{
+    private readonly IHostApplicationLifetime _applicationLifetime;
+    private int _currentIndex;
+    private byte[]? _payload;
+    
+    public TestActor(string persistenceId, int payloadSize, IHostApplicationLifetime applicationLifetime)
+    {
+        PersistenceId = persistenceId;
+        _applicationLifetime = applicationLifetime;
+
+        var log = Context.GetLogger();
+        
+        Recover<SnapshotOffer>(offer => _payload = (byte[])offer.Snapshot);
+        Recover<byte[]>(bytes => _payload = bytes);
+        Recover<RecoveryCompleted>(
+            _ =>
+            {
+                log.Info("Recovery Completed");
+                if (_payload == null)
+                {
+                    var rnd = new Random();
+                    _payload = new byte[payloadSize];
+                    for (var i = 0; i < payloadSize; i++)
+                    {
+                        _payload[i] = (byte)rnd.Next(0, 255);
+                    }
+                }
+            });
+        Command<SaveEvent>(_ =>
+            {
+                Persist(_payload,
+                    _ =>
+                    {
+                        _currentIndex++;
+                        if (_currentIndex % 5 == 0)
+                        {
+                            SaveSnapshot(_payload);
+                        }
+                    });
+            });
+        Command<SaveSnapshotSuccess>(
+            evt =>
+            {
+                DeleteMessages(evt.Metadata.SequenceNr - 1);
+            });
+        Command<DeleteMessagesSuccess>(
+            _ =>
+            {
+                log.Info("Messages deleted");
+            });
+        Command<DeleteMessagesFailure>(
+            fail =>
+            {
+                log.Error(fail.Cause, "Failed to delete messages");
+                _applicationLifetime.StopApplication();
+            });
+    }
+    
+    public override string PersistenceId { get; }
+}
