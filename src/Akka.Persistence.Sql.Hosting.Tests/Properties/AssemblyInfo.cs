40d7cf21026f451b344fd44fe77c85e27cd51956
Aaron Stannard <aaron@petabridge.com>
2025-10-23 20:53:34 -0500

    Register custom xunit test framework in Hosting.Tests assembly
    The Akka.Persistence.Sql.Hosting.Tests project was missing the TestFramework attribute
    registration that enables the custom SqlTestFramework, which provides platform-specific
    test skipping via [SkipWindows] and [SkipLinux] attributes.
    
    Created Properties/AssemblyInfo.cs with the TestFramework attribute to register the
    SqlTestFramework. This enables the Docker-based connectivity check tests to be properly
    skipped on Windows CI where Docker is unavailable.

---
+++ src/Akka.Persistence.Sql.Hosting.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------
+//  <copyright file="AssemblyInfo.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Xunit;
+
+[assembly:
+    TestFramework(
+        "Akka.Persistence.Sql.Tests.Common.Internal.Xunit.SqlTestFramework",
+        "Akka.Persistence.Sql.Tests.Common")]
