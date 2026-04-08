b9e7f1e85ab13cea1b01786318420254191da835
Gregorius Soedharmo <arkatufus@yahoo.com>
2026-04-08 23:10:42 +0700

    Fix missing SkipWindows attribute

--- src/Akka.Persistence.Sql.Tests/Query/SqlServer/TagTable/SqlServerQueryThrottleSpecs.cs
+++ src/Akka.Persistence.Sql.Tests/Query/SqlServer/TagTable/SqlServerQueryThrottleSpecs.cs
@@ -8,9 +8,15 @@ using Akka.Persistence.Sql.Config;
 using Akka.Persistence.Sql.Tests.Common.Containers;
 using Akka.Persistence.Sql.Tests.SqlServer;
 using Xunit;
+#if !DEBUG
+using Akka.Persistence.Sql.Tests.Common.Internal.Xunit;
+#endif
 
 namespace Akka.Persistence.Sql.Tests.Query.SqlServer.TagTable;
 
+#if !DEBUG
+[SkipWindows]
+#endif
 [Collection(nameof(SqlServerPersistenceSpec))]
 public class SqlServerQueryThrottleSpecs: QueryThrottleSpecsBase<SqlServerContainer>
 {
