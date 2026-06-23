ced91e584571c154263169eafeabbc54a0637de7
Aaron Stannard <aaron@petabridge.com>
2026-06-23 15:01:50 +0000

    test: add FromEnd TCK specs for all database implementations

---
+++ src/Akka.Persistence.Sql.Tests/Query/PostgreSql/TagTable/PostgreSqlFromEndOffsetSpec.cs
@@ -0,0 +1,27 @@
+// -----------------------------------------------------------------------
+//  <copyright file="PostgreSqlFromEndOffsetSpec.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Persistence.Sql.Config;
+using Akka.Persistence.Sql.Tests.Common.Containers;
+using Akka.Persistence.Sql.Tests.Common.Query;
+using Akka.Persistence.Sql.Tests.PostgreSql;
+using Xunit;
+#if !DEBUG
+using Akka.Persistence.Sql.Tests.Common.Internal.Xunit;
+#endif
+
+namespace Akka.Persistence.Sql.Tests.Query.PostgreSql.TagTable
+{
+#if !DEBUG
+    [SkipWindows]
+#endif
+    [Collection(nameof(PostgreSqlPersistenceSpec))]
+    public class PostgreSqlFromEndOffsetSpec : BaseFromEndOffsetSpec<PostgreSqlContainer>
+    {
+        public PostgreSqlFromEndOffsetSpec(ITestOutputHelper output, PostgreSqlContainer fixture)
+            : base(TagMode.TagTable, output, nameof(PostgreSqlFromEndOffsetSpec), fixture) { }
+    }
+}
